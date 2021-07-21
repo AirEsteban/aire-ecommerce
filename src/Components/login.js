2ee9923085a0d67b85ae4105897ef5154e3d58f6
Esteban <airascaesteban@gmail.com>
2021-07-21 14:13:33 -0300

    Login finished, must try to do the responsive for the navbar

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import NavBar from './navbar';
-
+import '../css/login.scss';
+import logo from '../images/react2.png';
 function Login(){
     return (
-    <div>
-        <NavBar />
-        <h1>soy login</h1>
-    </div>
+        <div class="container">
+            <div class="row margin-row">
+                <div class="col-4"></div>
+                <div class="col-4 box">
+                    <div class="box-content">
+                    <div class="row">
+                        <div class="text-center">
+                            <img src={logo} alt="..." class="rounded"/>
+                        </div>
+                    </div>
+                        <div class="mb-3 mt-3 row">
+                            <label for="inputUsername" class="login-lbl">Username</label>
+                            <div class="col-sm-12">
+                                <input type="input" class="form-control login-inp" id="inputUsername" />
+                            </div>
+                        </div>
+                        <br/>
+                        <div class="mb-5 row">
+                            <label for="inputPassword" class="login-lbl">Password</label>
+                            <div class="col-sm-12">
+                                <input type="password" class="form-control login-inp" id="inputPassword" />
+                            </div>
+                        </div>
+                        <br/>
+                        <div class="mb-5 row">
+                            <button class="btn btn-info mb-3">Log In!</button>
+                            <span>Don't have an account?<a href="/signup" class="login-lbl"> Sign up</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4"></div>
+            </div>
+      </div>
     )
 }export default Login;
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -10,34 +10,35 @@ import {
 function NavBar(){
     return (
     <Router>
-        <div className='nav-color'>
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link to='/man' className="active nav-color-text" aria-current="page">MAN</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/woman' className="active nav-color-text" aria-current="page" href="#">WOMAN</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/kids' className="active nav-color-text" aria-current="page" href="#">KIDS</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/sport' className="active nav-color-text" aria-current="page" href="#">SPORT</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/sale' className="active nav-color-text" aria-current="page" href="#">SALE</Link>
-                            </li>
-                        </ul>
-                        <form class="d-flex">
-                            <Link to='/login'><button class="btn btn-info" type="submit">LOGIN</button></Link>
-                        </form>
-                    </div>
+        <nav className="nav-color navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to='/man' className="active nav-color-text" aria-current="page">MAN</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/woman' className="active nav-color-text" aria-current="page" href="#">WOMAN</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/kids' className="active nav-color-text" aria-current="page" href="#">KIDS</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sport' className="active nav-color-text" aria-current="page" href="#">SPORT</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sale' className="active nav-color-text" aria-current="page" href="#">SALE</Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <Link to='/login'><button class="btn btn-info" type="submit">LOGIN</button></Link>
+                    </form>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
         <Switch>
           <Route exact path="/login">
             <Login />
